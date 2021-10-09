26623ad37bd5c0aa2e943067fbbf3e0b2a0d73a7
LeonidYatsina <lenap9106@gmail.com>
2021-10-09 12:33:49 +0300

    Обновил дз

--- homework.1/10.09.py
+++ homework.1/10.09.py
@@ -1,7 +1,30 @@
-def show(func):
-    def new_func(*args, **kwargs):
+import random
+import string
+
+names=[]
+
+def decorator(func):
+    def inner(*args, **kwargs):
         print('Running function: ', func.__name__)
         print('Positional arguments are: ', args)
         print('Keyword arguments are: ', kwargs)
-        
-    return new_func
+        result = func(*args, **kwargs)
+        print('Result: ', result)
+        return result
+    return inner
+
+
+def gen_names(count_names):
+    for i in range(count_names):
+        name = ''
+        name = chr(random.randint(65, 90))
+        rnd_num = random.randint(3, 10)
+        for u in range(rnd_num):
+            name += chr(random.randint(97, 122))
+        names.append(name)
+    return names
+
+print('Введите количество имен:')
+n = int(input())
+b = decorator(gen_names)
+print(b(n))
