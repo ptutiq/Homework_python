83fc74ef7f55b48cb22c1da28a020c15456cccb3
LeonidYatsina <lenap9106@gmail.com>
2021-10-09 12:36:43 +0300

    Обновил дз

--- homework.1/20.09.py
+++ homework.1/20.09.py
@@ -1,20 +1,10 @@
-set_1 = {1,2,3,4,5}
-set_2 = {3,4,5,6,54,6,5,11,11}
-f = set_1.isdisjoint(set_2)
-print(f)
+def unique(array):
+    return [i for i in set(array) if array.count(i) == 1]
 
-print(set_1 == set_2)
 
-print(set_1.issubset(set_2))
+def intersection(list_1, list_2):
+    return [i for i in set.intersection(set(list_1), set(list_2))]
 
-print(set_1.issuperset(set_2))
 
-print(set_1.union(set_2))
-
-print(set_1.intersection(set_2))
-
-print(set_1.difference(set_2))
-
-print(set_1.symmetric_difference(set_2))
-
-print(set_1.copy)
+print(unique([1, 2, 2, 2, 4, 5, 1, 4, 10, 100, 11, 11]))
+print(intersection([1, 2, 3, 4, 5, 6, 7, 8], [1, 2, 3, 6, 7, 8]))
